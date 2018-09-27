6464e1f0fd510f8efb13df4407d5696c7bf13af9
htoutlook <34896805+htoutlook@users.noreply.github.com>
2018-09-27 12:47:33 -0500

    Delete ginput.py

--- ginput.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import, division, print_function
-
-def get_input(prompt=''):
-	try:
-		line = raw_input(prompt)
-	except NameError:
-		line = input(prompt)
-	return line
